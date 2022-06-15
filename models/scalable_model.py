dd496d9acf79d6c313252e82f9a24307b1715ac6
Cyrille Pittet <cyrille.pittet@gmail.com>
2022-06-15 10:09:15 +0200

    Updated scalable model with initialization

--- models/scalable_model.py
+++ models/scalable_model.py
@@ -8,7 +8,7 @@ from math import sqrt
 # scale = torch.linspace(start,30,50)
     
 class ModularModel(nn.Module):
-    def __init__(self,scale):
+    def __init__(self,scale, init=False):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 3, 5)
         self.pool = nn.MaxPool2d(2, 2)
@@ -16,6 +16,19 @@ class ModularModel(nn.Module):
         self.fc1 = nn.Linear(3 * 5 * 5, max(10,int(scale*120)))
         self.fc2 = nn.Linear(max(10,int(scale*120)), max(10,int(sqrt(scale*120))))
         self.fc3 = nn.Linear(max(10,int(sqrt(scale*120))), 10)
+
+        if init:
+            nn.init.xavier_normal_(self.conv1.weight)
+            nn.init.xavier_normal_(self.conv2.weight)
+            nn.init.xavier_normal_(self.fc1.weight)
+            nn.init.xavier_normal_(self.fc2.weight)
+            nn.init.xavier_normal_(self.fc3.weight)
+
+            self.conv1.bias.data.fill_(0.01)
+            self.conv2.bias.data.fill_(0.01)
+            self.fc1.bias.data.fill_(0.01)
+            self.fc2.bias.data.fill_(0.01)
+            self.fc3.bias.data.fill_(0.01)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
